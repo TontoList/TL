953bc4647c805e942918bc058ded0e468f47d8ce
DonTonto <dgamer90909@gmail.com>
2025-10-09 20:22:55 +0200

    Update routes.js

--- js/routes.js
+++ js/routes.js
@@ -2,9 +2,8 @@ import List from './pages/List.js';
 import Leaderboard from './pages/Leaderboard.js';
 import Roulette from './pages/Roulette.js';
 import Packs from './pages/Packs.js';
-
 export default [
-    { path: '/list', component: List },
+    { path: '/', component: List },
     { path: '/leaderboard', component: Leaderboard },
     { path: '/roulette', component: Roulette },
     { path: '/packs', component: Packs },
