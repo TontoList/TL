7c41573e879eef57930ed9ca9c476d67dc6d0794
DonTonto <dgamer90909@gmail.com>
2025-10-10 16:57:28 +0200

    Update List.js

--- js/pages/List.js
+++ js/pages/List.js
@@ -102,25 +102,25 @@ export default {
                     <p>
                         Achieved the record without using hacks (however, CBF and FPS bypass are both allowed (FPS Bypass up to 360fps)).
                     </p>
-                    <p>
+                    <p style="padding: 10px; background-color: black; border-radius: 8px;">
                         Achieved the record on the level that is listed on the site - please check the level ID before you submit a record
                     </p>
-                    <p style="padding: 10px; background-color: black; border-radius: 8px;">
+                    <p>
                         Have either source audio or clicks/taps in the video. Edited audio only does not count
                     </p>
-                    <p>
+                    <p style="padding: 10px; background-color: black; border-radius: 8px;">
                         The recording must have a previous attempt and entire death animation shown before the completion, unless the completion is on the first attempt. Everyplay records are exempt from this
                     </p>
-                    <p style="padding: 10px; background-color: black; border-radius: 8px;">
+                    <p>
                         The recording must also show the player hit the endwall, or the completion will be invalidated.
                     </p>
-                    <p>
+                    <p style="padding: 10px; background-color: black; border-radius: 8px;">
                         Do not use secret routes or bug routes
                     </p>
-                    <p style="padding: 10px; background-color: black; border-radius: 8px;">
+                    <p>
                         Do not use easy modes, only a record of the unmodified level qualifies
                     </p>
-                    <p>
+                    <p style="padding: 10px; background-color: black; border-radius: 8px;">
                         Once a level falls onto the Legacy List, we accept records for it for 24 hours after it falls off, then afterwards we never accept records for said level
                     </p>
                 </div>
