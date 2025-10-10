31e8c890585805883bfd168317e86c0a3aecb7cc
DonTonto <dgamer90909@gmail.com>
2025-10-10 16:55:34 +0200

    Update List.js

--- js/pages/List.js
+++ js/pages/List.js
@@ -96,7 +96,7 @@ export default {
                         </ol>
                     </template>
                     <h3><u>Submission Requirements</u></h3>
-                    <p style="background-color: black; border-radius: 8px;">
+                    <p style="padding: 10px; background-color: black; border-radius: 8px;">
                         The level must be capable of obtaining a proper scale rating within the Tonto Scale which ranges from 0 to 100.
                     </p>
                     <p>
