d7c111e7f8e5c4cd1bd0b55e21d19e10e25ddf10
DonTonto <dgamer90909@gmail.com>
2025-10-19 21:58:06 +0200

    Update routes.js

--- js/routes.js
+++ js/routes.js
@@ -1,7 +1,7 @@
 import List from './pages/List.js';
 import Leaderboard from './pages/Leaderboard.js';
 import Roulette from './pages/Roulette.js';
-
+import TontoScaleList from './pages/TontoScaleList.js';
 export default [
     { path: '/', component: List },
     { path: '/leaderboard', component: Leaderboard },
