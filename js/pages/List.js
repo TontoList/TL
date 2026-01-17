f52d37ccfe81526ea8de24fd73566c3194a3fe6e
DonTonto <dgamer90909@gmail.com>
2026-01-17 12:37:26 +0100

    Remove Tonto Scale from List component
    Removed Tonto Scale display from the list.

--- js/pages/List.js
+++ js/pages/List.js
@@ -50,10 +50,6 @@ export default {
                             <div class="type-title-sm">ID</div>
                             <p>{{ level.id || 'NaN' }}</p>
                         </li>
-                        <li>
-                            <div class="type-title-sm">Tonto Scale</div>
-                            <p>{{ level.tontoscale || '0' }}</p>
-                        </li>
                     </ul>
                     <h2>Records</h2>
                     <p v-if="selected + 1 <= 75"><strong>{{ level.percentToQualify }}%</strong> or better to qualify</p>
