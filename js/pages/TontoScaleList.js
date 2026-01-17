7c94f1736e8275c68643396aa98cf0cc4a1d8382
DonTonto <dgamer90909@gmail.com>
2026-01-17 12:44:14 +0100

    Update TontoScaleList.js

--- js/pages/TontoScaleList.js
+++ js/pages/TontoScaleList.js
@@ -21,7 +21,7 @@ export default {
         </main>
         <main v-else class="page-list">
             <div class="list-container">
-                <table class="list" v-if="tontoscalelist">
+                <table class="tontoscalelist" v-if="tontoscalelist">
                     <tr v-for="([level, err], i) in tontoscalelist">
                         <td class="rank">
                             <p v-if="i + 1 <= 75" class="type-label-lg">#{{ i + 1 }}</p>
